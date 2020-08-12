f64487625f71a8bf43d1be93099bd59c11b20ed7
Akash_Pol <65165883+02akashpol@users.noreply.github.com>
2020-08-12 23:46:15 +0530

    Assignment file is uploaded.

---
+++ Day_8/Assi8.5.js
@@ -0,0 +1,31 @@
+
+let demo01 = { };
+//undefined
+
+let EmpInfo = {
+    fName : "Akash",
+};
+//undefined
+
+let EmpInfo = {
+    "fName" : "Akash",
+};
+//undefined
+
+let EmpInfo = {
+    'fName' : "Akash",
+};
+//undefined
+
+let empInfo = {
+    id : 100,
+    fName : "Akash",
+    "last-name" : "Pol",
+    'City' : "Satara",
+};
+//undefined
+
+let list = [
+    container = [1,2,3],
+];
+//undefined
